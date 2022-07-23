204c30e9844a704115cf4b40ac1b47fd278be57a
rupak-reddy <96642521+rupak-reddy@users.noreply.github.com>
2022-07-23 17:43:53 +0530

    Add files via upload

---
+++ myproject/info.py
@@ -0,0 +1,8 @@
+from pickle import TRUE
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'projectd746@gmail.com'
+EMAIL_HOST_PASSWORD = 'tkmhjgunwxhrpzhi'
+EMAIL_PORT = 587
